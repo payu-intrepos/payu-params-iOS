090713ba6fd2360d6b68149bc5b3a6138c60f465
Shubham Garg <91garg.shubham@gmail.com>
2022-01-06 15:25:05 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,5 @@ let package = Package(
     ],
     targets: [
             .binaryTarget(name: "PayUIndia-PayUParams", path: "./PayUParamsKit/PayUParamsKit.xcframework")
-        ],
-    exclude: ["./PayUIndia-PayUParams-Release.sh", "./PayUIndia-PayUParams.podspec"]
+        ]
 )
